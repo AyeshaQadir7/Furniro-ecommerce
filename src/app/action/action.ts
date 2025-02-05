64fc74fb908ea41d752a5ea887e4b6162708f866
Ayeha Qadir7 <ayeshaabdulqadir07@gmail.com>
2025-02-05 12:25:14 +0500

    deployment error solved

--- src/app/action/action.ts
+++ src/app/action/action.ts
@@ -1,14 +1,15 @@
 import { Product } from "../types/product";
 
 export const addToCart = (product: Product) => {
-  const cart: Product[] = JSON.parse(localStorage.getItem("cart") || "[]");
+  const cart: Product[] =
+    JSON.parse(localStorage.getItem("cart") || "[]") || [];
 
-  const existingProductIndex = cart.findIndex(
-    (items) => items._id === product._id
-  );
+  const existingProduct = cart.find((item) => item._id === product._id);
 
-  if (existingProductIndex > -1) {
-    cart[existingProductIndex].inventory += 1;
+  if (existingProduct) {
+    if (existingProduct.inventory !== undefined) {
+      existingProduct.inventory += 1;
+    }
   } else {
     cart.push({ ...product, inventory: 1 });
   }
@@ -17,22 +18,25 @@ export const addToCart = (product: Product) => {
 };
 
 export const removeFromCart = (productId: string) => {
-  let cart: Product[] = JSON.parse(localStorage.getItem("cart") || "[]");
+  const cart: Product[] =
+    JSON.parse(localStorage.getItem("cart") || "[]") || [];
 
-  cart = cart.filter((product) => product._id !== productId);
-  localStorage.setItem("cart", JSON.stringify(cart));
+  const updatedCart = cart.filter((product) => product._id !== productId);
+  localStorage.setItem("cart", JSON.stringify(updatedCart));
 };
 
-export const updateCartQuantity = (productId: string, qunatity: number) => {
-  const cart: Product[] = JSON.parse(localStorage.getItem("cart") || "[]");
-  const productIndex = cart.findIndex((product) => product._id === productId);
+export const updateCartQuantity = (productId: string, quantity: number) => {
+  const cart: Product[] =
+    JSON.parse(localStorage.getItem("cart") || "[]") || [];
 
-  if (productIndex > -1) {
-    cart[productIndex].inventory = qunatity;
+  const product = cart.find((item) => item._id === productId);
+
+  if (product) {
+    product.inventory = quantity;
     localStorage.setItem("cart", JSON.stringify(cart));
   }
 };
 
 export const getCartItems = (): Product[] => {
-  return JSON.parse(localStorage.getItem("cart") || "[]");
+  return JSON.parse(localStorage.getItem("cart") || "[]") || [];
 };
